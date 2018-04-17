5733f10fd6453faefa5838733bf8faec2f2e97bd
AntonEmery <antonemery@gmail.com>
2018-04-17 11:13:00 -0700

    project preview images flexing

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,7 +5,14 @@ class Projects extends React.Component {
 
   render() {
     return (
-      <div>Projects here</div>
+      <div className="projects-container">
+        <p className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
+        <p className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
+        <p className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
+      </div>
     )
   }
 }
