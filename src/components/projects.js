78fe3ed1c29a718da6bd187cda4cb8cab713c8a7
AntonEmery <antonemery@gmail.com>
2018-04-17 11:39:56 -0700

    project description text hovering correctly

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,12 +6,17 @@ class Projects extends React.Component {
   render() {
     return (
       <div className="projects-container">
-        <p className="project"><img
-        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
-        <p className="project"><img
-        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
-        <p className="project"><img
-        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></p>
+        <a href="#" className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" />
+        <div className="project-description">
+          <h5>I fly alaska</h5>
+          <p>React, Webpack, SCSS</p>
+        </div>
+        </a>
+        <a href="#" className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></a>
+        <a href="#" className="project"><img
+        className="img-responsive img-responsive--project" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></a>
       </div>
     )
   }
