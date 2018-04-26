56117560157604ae5892ad1fc93a753484e37d1c
AntonEmery <antonemery@gmail.com>
2018-04-26 11:58:03 -0700

    displaying project data from graphql query

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,70 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-class Projects extends React.Component {
-
-  render() {
+function Projects({title, tagline, imageUrl}){
     return (
-      <div className="projects-container">
-        <a href="#" className="project"><img
-        className="img-responsive" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" />
-        <div className="project-description">
-          <h5 className="project-text">I fly alaska</h5>
-          <p className="project-text">React, Webpack, SCSS</p>
-        </div>
+      <div className="project">
+        <a href="#"><img
+        className="img-responsive img-responsive--project" src={imageUrl} />
+          <div className="project-description">
+            <h5 className="project-text">{title}</h5>
+            <p className="project-text">{tagline}</p>
+          </div>
         </a>
-        <a href="#" className="project"><img
-        className="img-responsive" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></a>
-        <a href="#" className="project"><img
-        className="img-responsive" src="http://www.antonemery.com/wp-content/uploads/2016/08/iflyalaska.png" /></a>
       </div>
     )
-  }
 }
 
 export default Projects;
-
-export const websiteQuery = graphql`
-{
-  allWordpressWpPortfolioItem {
-    edges {
-     node {
-      wordpress_id
-      title
-      content
-      acf {
-        livelink
-        tagline
-        imageurl {
-          source_url
-        }
-      }
-      }
-    }
-  }
-}`
-
-export const webappQuery = graphql`
-{
-	allWordpressWpWebapp{
-    edges{
-     node{
-      wordpress_id
-      title
-      content
-      acf {
-        githublink
-        livelink
-        tagline
-        imageurl {
-          source_url
-        }
-      }
-    }
-  }
-  }
-}`
-
-
-
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,68 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Projects from '../components/projects'
 
-const IndexPage = () => (
+const IndexPage = props => {
+  console.log(props)
+  const webProjects = props.data.allWordpressWpPortfolioItem.edges;
+  const test = webProjects.map((project, index) => {
+    return <Projects
+      key={index}
+      title={project.node.title}
+      tagline={project.node.acf.tagline}
+      imageUrl={project.node.acf.imageurl.source_url}
+    />
+  })
+  return (
   <div>
     <p>Lorem ipsum dolor amet activated charcoal hella hexagon, beard etsy put a bird on it wolf. Heirloom farm-to-table everyday carry, glossier meditation seitan live-edge succulents four loko occupy cardigan keytar woke pinterest. Banjo prism tacos pop-up celiac. Kogi hot chicken scenester gastropub enamel pin meggings cred. Bitters biodiesel jean shorts, austin live-edge man braid 3 wolf moon blue bottle pinterest truffaut intelligentsia pour-over whatever kogi VHS.</p>
-    <Projects />
+    <div className="projects-container">
+      {test}
+    </div>
   </div>
-)
+  )
+}
 
 export default IndexPage
+
+export const websiteQuery = graphql`
+  query PortfolioItem {
+    allWordpressWpPortfolioItem {
+    edges {
+     node {
+      wordpress_id
+      title
+      content
+      acf {
+        livelink
+        tagline
+        imageurl {
+          source_url
+        }
+      }
+      }
+    }
+  }
+}
+`
+
+// export const webappQuery = graphql`
+//   query WebApp {
+// 	  allWordpressWpWebapp {
+//     edges {
+//      node {
+//       wordpress_id
+//       title
+//       content
+//       acf {
+//         githublink
+//         livelink
+//         tagline
+//         imageurl {
+//           source_url
+//         }
+//       }
+//     }
+//   }
+//   }
+// }
+// `
