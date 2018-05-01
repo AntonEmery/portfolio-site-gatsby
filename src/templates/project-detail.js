3867386675bd38c24e10ed05882eacdb524dff7b
AntonEmery <antonemery@gmail.com>
2018-04-30 19:49:03 -0700

    styled project details page

--- src/templates/project-detail.js
+++ src/templates/project-detail.js
@@ -6,13 +6,17 @@ class ProjectDetail extends Component {
   render() {
     const { title, content, acf } = this.props.data.wordpressWpPortfolioItem
     return (
-      <div>
-        <p>Project Details</p>
-        <p>{title}</p>
-        <p>{acf.description}</p>
-        <p>Technologies Used: {acf.tagline}</p>
-        <a href={acf.livelink} target="_blank">Live Project</a>
-        <img src={acf.imageurl.source_url} alt="project screen shot"/>
+      <div className="project-details">
+        <div>
+          <p>Project Details</p>
+          <p>{title}</p>
+          <p>{acf.description}</p>
+          <p>Technologies Used: {acf.tagline}</p>
+          <a href={acf.livelink} target="_blank">Live Project</a>
+        </div>
+        <div>
+          <img className="img-responsive" src={acf.imageurl.source_url} alt="project screen shot"/>
+        </div>
       </div>
     );
   }
