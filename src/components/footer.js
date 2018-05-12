bd446882a2a29f28522303fc66450e520def3037
AntonEmery <antonemery@gmail.com>
2018-05-11 18:11:43 -0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,7 +22,7 @@ const Footer = () => (
         <FaEnvelope />
       </a>
     </div>
-    <p className="gatsby">Built with &hearts; using <a href="https://www.gatsbyjs.org/" target="_blank" className="text-link">Gatsby.js</a></p>
+    <p className="gatsby">Built with &hearts; using <a href="https://www.gatsbyjs.org/" target="_blank" className="text-link">Gatsby.js</a> and <a href="https://wordpress.org/" target="_blank" className="text-link">WordPress</a></p>
   </footer>
 )
 
