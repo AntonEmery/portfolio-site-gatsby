f684b9a999296dcf04c75fb24f1bf95f588f2da6
AntonEmery <antonemery@gmail.com>
2018-08-20 16:20:38 -0700

    added mobile nav toggle

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import { NavLink } from 'react-router-dom';
 import Toggle from './toggle';
 
-const Nav = () => (
-  <Toggle>
-    {({ on, toggle }) => (
-      <div>
-        <p onClick={toggle}>MENU</p>
-        {on &&
-        <nav className="nav-list">
-          <NavLink activeClassName="nav-item--is-active" exact={true} to="/" className="nav-item">HOME</NavLink>
-          <NavLink activeClassName="nav-item--is-active" to="/about" className="nav-item">ABOUT</NavLink>
-          <NavLink activeClassName="nav-item--is-active" to="/contact" className="nav-item">CONTACT</NavLink>
-          <NavLink activeClassName="nav-item--is-active" to="http://antonemery.com/antonemeryresume.pdf" target="_blank" className="nav-item">RESUME</NavLink>
-        </nav>}
-      </div>
-      )}
-  </Toggle>
-)
+class Nav extends Component {
+
+  render() {
+    return (
+      <Toggle>
+        {({ on, toggle }) => (
+          <div>
+            <nav className="nav-list">
+              <NavLink activeClassName="nav-item--is-active" exact={true} to="/" className="nav-item">HOME</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="/about" className="nav-item">ABOUT</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="/contact" className="nav-item">CONTACT</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="http://antonemery.com/antonemeryresume.pdf" target="_blank" className="nav-item">RESUME</NavLink>
+            </nav>
+            <p className="toggle-menu" onClick={toggle}>TOGGLE MENU</p>
+            {on &&
+            <nav className="nav-list-mobile">
+              <NavLink activeClassName="nav-item--is-active" exact={true} to="/" className="nav-item">HOME</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="/about" className="nav-item">ABOUT</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="/contact" className="nav-item">CONTACT</NavLink>
+              <NavLink activeClassName="nav-item--is-active" to="http://antonemery.com/antonemeryresume.pdf" target="_blank" className="nav-item">RESUME</NavLink>
+            </nav>}
+          </div>
+          )}
+      </Toggle>
+    )
+  }
+}
 
 export default Nav;
