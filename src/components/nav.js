f03d3afee3ac0c7625ade6dd1ea611fee9c2d556
AntonEmery <antonemery@gmail.com>
2019-05-10 11:19:43 -0700

    update contact page

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,6 +4,7 @@ import Toggle from './toggle';
 import onRouteUpdate from '../../gatsby-browser';
 
 class Nav extends Component {
+
   render() {
     return (
       <Toggle>
@@ -18,7 +19,7 @@ class Nav extends Component {
             <p className="toggle-menu" onClick={toggle}>MENU</p>
             {on &&
             <nav className="nav-list-mobile">
-              <NavLink onClick={toggle} onBlur={toggle} activeClassName="nav-item--is-active" exact={true} to="/" className="nav-item">HOME</NavLink>
+              <NavLink onClick={toggle} activeClassName="nav-item--is-active" exact={true} to="/" className="nav-item">HOME</NavLink>
               <NavLink onClick={toggle} activeClassName="nav-item--is-active" to="/about" className="nav-item">ABOUT</NavLink>
               <NavLink onClick={toggle} activeClassName="nav-item--is-active" to="/contact" className="nav-item">CONTACT</NavLink>
               <NavLink onClick={toggle} activeClassName="nav-item--is-active" to="http://antonemery.com/antonemeryresume.pdf" target="_blank" className="nav-item">RESUME</NavLink>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,12 +5,11 @@ import FaEnvelope from 'react-icons/lib/fa/envelope'
 
 
 const Contact = () => (
-<div className="contact">
-<p>The best ways to get ahold of me are phone or email.</p>
-<p>Want to hire me for a project or just talk about web development, music, or anything else? Feel free to reach out!</p>
-<p><a href="tel:971-238-9576" className="text-link">971-238-9576</a></p>
-<p><a href="mailto:antonemery@gmail.com" className="text-link">antonemery@gmail.com</a></p>
-</div>
+  <div className="contact">
+    <p>The best way to get ahold of me is via email, or reach out to me on <a className="text-link" href="http://www.twitter.com/antonemery">Twitter.</a></p>
+    <p>Want to hire me for a project or just talk about web development, music, or anything else? Feel free to reach out!</p>
+    <p><a href="mailto:antonemery@gmail.com" className="text-link">antonemery@gmail.com</a></p>
+  </div>
 )
 
 export default Contact
