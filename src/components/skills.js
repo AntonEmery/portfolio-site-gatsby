f0c28768bda925a56d490acb52a7dc9408ee5375
AntonEmery <antonemery@gmail.com>
2019-12-10 21:54:22 -0800

    remove blog link

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Skills = () => (
   <section className="skill-section">
     <h2>Skills</h2>
-    <ul>
+    <ul className="skills-list">
       <li>Strong front end engineer. Good vanilla Javascript skills</li>
       <li>Expertise with React, Mithril, and other front end frameworks</li>
       <li>Good ability with Node</li>
